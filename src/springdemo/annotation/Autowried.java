e22bf7df9082a244c906d22433afec1f9737e362
Xie <2951542805@qq.com>
2019-12-30 23:15:09 +0800

    注解测试

--- src/springdemo/annotation/Autowried.java
+++ src/springdemo/annotation/Autowried.java
@@ -6,9 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface RequestMapper {
-	
+@Target(ElementType.FIELD)
+public @interface Autowried {
 	String value() default "";
-	
 }
